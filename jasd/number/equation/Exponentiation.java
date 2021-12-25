93ef931bb7d27c65fb703fc0ae0ca5c9610ba24b
JASD <86506578+JASDtubz@users.noreply.github.com>
2021-12-25 02:42:12 -0600

    Create Exponentiation.java

---
+++ jasd/number/equation/Exponentiation.java
@@ -0,0 +1,36 @@
+package jasd.number.equation;
+
+public class Exponentiation extends Operation
+{
+    public Exponentiation(double i, double j) { super(i, j); }
+
+    public Exponentiation(double i, Operation o) { super(i, o); }
+
+    public Exponentiation(Operation o, double i) { super(o, i); }
+
+    public Exponentiation(Operation o, Operation p) { super(o, p); }
+
+    @Override
+    public double evaluate()
+    {
+        switch (super.c)
+        {
+            case 0: return StrictMath.pow(super.a, super.b);
+            case 1: return StrictMath.pow(super.a, super.p.evaluate());
+            case 2: return StrictMath.pow(super.o.evaluate(), super.b);
+            default: return StrictMath.pow(super.o.evaluate(), super.p.evaluate());
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        switch (super.c)
+        {
+            case 0: return String.format("(%s^%s)", super.a, super.b);
+            case 1: return String.format("(%s^%s)", super.a, super.p);
+            case 2: return String.format("(%s^%s)", super.o, super.b);
+            default: return String.format("(%s^%s)", super.o, super.p);
+        }
+    }
+}
