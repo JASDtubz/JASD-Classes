2edb98068b6f85431aa49f6914be474edeea9a00
JASD <86506578+JASDtubz@users.noreply.github.com>
2021-12-25 02:41:07 -0600

    Create Multiplication.java

---
+++ jasd/number/equation/Multiplication.java
@@ -0,0 +1,36 @@
+package jasd.number.equation;
+
+public class Multiplication extends Operation
+{
+    public Multiplication(double i, double j) { super(i, j); }
+
+    public Multiplication(double i, Operation o) { super(i, o); }
+
+    public Multiplication(Operation o, double i) { super(o, i); }
+
+    public Multiplication(Operation o, Operation p) { super(o, p); }
+
+    @Override
+    public double evaluate()
+    {
+        switch (super.c)
+        {
+            case 0: return super.a * super.b;
+            case 1: return super.a * super.p.evaluate();
+            case 2: return super.o.evaluate() * super.b;
+            default: return super.o.evaluate() * super.p.evaluate();
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        switch (super.c)
+        {
+            case 0: return String.format("(%s*%s)", super.a, super.b);
+            case 1: return String.format("(%s*%s)", super.a, super.p);
+            case 2: return String.format("(%s*%s)", super.o, super.b);
+            default: return String.format("(%s*%s)", super.o, super.p);
+        }
+    }
+}
