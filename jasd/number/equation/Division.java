c99fa651bc481573081f06c43296d7e84c522388
JASD <86506578+JASDtubz@users.noreply.github.com>
2021-12-25 02:41:31 -0600

    Create Division.java

---
+++ jasd/number/equation/Division.java
@@ -0,0 +1,36 @@
+package jasd.number.equation;
+
+public class Division extends Operation
+{
+    public Division(double i, double j) { super(i, j); }
+
+    public Division(double i, Operation o) { super(i, o); }
+
+    public Division(Operation o, double i) { super(o, i); }
+
+    public Division(Operation o, Operation p) { super(o, p); }
+
+    @Override
+    public double evaluate()
+    {
+        switch (super.c)
+        {
+            case 0: return super.a / super.b;
+            case 1: return super.a / super.p.evaluate();
+            case 2: return super.o.evaluate() / super.b;
+            default: return super.o.evaluate() / super.p.evaluate();
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        switch (super.c)
+        {
+            case 0: return String.format("(%s/%s)", super.a, super.b);
+            case 1: return String.format("(%s/%s)", super.a, super.p);
+            case 2: return String.format("(%s/%s)", super.o, super.b);
+            default: return String.format("(%s/%s)", super.o, super.p);
+        }
+    }
+}
