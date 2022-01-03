0deaceda1ae93db28e43eac73f2d3df1694b664f
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-01-03 13:54:15 -0600

    Create VariableKey.java

---
+++ jasd/number/equation/VariableKey.java
@@ -0,0 +1,17 @@
+package jasd.number.equation;
+
+public class VariableKey
+{
+    private String s;
+    private double d;
+
+    public VariableKey(String s, double d)
+    {
+        this.s = s;
+        this.d = d;
+    }
+
+    public String getS() { return this.s; }
+
+    public double getD() { return this.d; }
+}
