f0d7df4907ec3b23c3ca4028836bf903326dbe2d
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-01-03 13:57:57 -0600

    Update Logarithm.java

--- jasd/number/logarithm/Logarithm.java
+++ jasd/number/logarithm/Logarithm.java
@@ -6,7 +6,7 @@ public class Logarithm
 
 	public double lg(x) { return Math.log(x); }
 
-	public double ln(x) { return Math.exp(x); }
+	public double ln(x) { return this.logBase(Math.E, x); }
 
 	public double lb(x) { return this.logBase(2, x); }
 }
