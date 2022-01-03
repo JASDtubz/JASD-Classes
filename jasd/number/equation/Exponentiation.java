0af56e3ae3428ac0055474a5eb2077a7c61bf4a5
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-01-03 13:48:38 -0600

    Update Exponentiation.java

--- jasd/number/equation/Exponentiation.java
+++ jasd/number/equation/Exponentiation.java
@@ -11,7 +11,7 @@ public class Exponentiation extends Operation
     public Exponentiation(Operation o, Operation p) { super(o, p); }
 
     @Override
-    public double evaluate()
+    double evaluate()
     {
         switch (super.c)
         {
@@ -33,4 +33,7 @@ public class Exponentiation extends Operation
             default: return String.format("(%s^%s)", super.o, super.p);
         }
     }
+
+    @Override
+    void setValue(VariableKey vk) { }
 }
