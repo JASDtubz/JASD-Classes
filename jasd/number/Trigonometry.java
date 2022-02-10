c871e4e6035e8ccb498611c52031c9af0e52f695
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-02-10 08:57:28 -0600

    Update Trigonometry.java

--- jasd/number/Trigonometry.java
+++ jasd/number/Trigonometry.java
@@ -2,7 +2,7 @@ package jasd.number;
 
 import jasd.number.Logarithm;
 
-public class MoreTrig
+public class Trigonometry
 {	
     public static double sin(double x) { return Math.sin(x); }
 
@@ -22,35 +22,35 @@ public class MoreTrig
 
     public static double coversin(double x) { return 1 - Math.sin(x); }
 
-    public static double haversin(double x) { return 0.5 * this.versin(x); }
+    public static double haversin(double x) { return 0.5 * Trigonometry.versin(x); }
 
-    public static double exsec(double x) { return this.sec(x) - 1; }
+    public static double exsec(double x) { return Trigonometry.sec(x) - 1; }
 
-    public static double excsc(double x) { return this.exsec(Math.PI / 2 - x); }
+    public static double excsc(double x) { return Trigonometry.exsec(Math.PI / 2 - x); }
 
     public static double sinh(double x) { return (Math.pow(Math.E, x) - Math.pow(Math.E, -x)) / 2; }
 
     public static double cosh(double x) { return (Math.pow(Math.E, x) + Math.pow(Math.E, -x)) / 2; }
 
-    public static double tanh(double x) { return this.sinh(x) / this.cosh(x); }
+    public static double tanh(double x) { return Trigonometry.sinh(x) / Trigonometry.cosh(x); }
 
-    public static double coth(double x) { return this.cosh(x) / this.sinh(x); }
+    public static double coth(double x) { return Trigonometry.cosh(x) / Trigonometry.sinh(x); }
 
-    public static double sech(double x) { return 1 / this.cosh(x); }
+    public static double sech(double x) { return 1 / Trigonometry.cosh(x); }
 
-    public static double csch(double x) { return 1 / this.sinh(x); }
+    public static double csch(double x) { return 1 / Trigonometry.sinh(x); }
 
     public static double arsinh(double x) { return Logarithm.ln(x + Math.sqrt(x * x + 1)); }
 
     public static double arcosh(double x) { return Logarithm.ln(x + Math.sqrt(x * x - 1)); }
 
-    public static double artanh(double x) { return 0.5 * this.l.ln((x + 1) / (x - 1)); }
+    public static double artanh(double x) { return 0.5 * Logarithm.ln((x + 1) / (x - 1)); }
 
-    public static double arcoth(double x) { return 0.5 * this.l.ln((x + 1) / (x - 1)); }
+    public static double arcoth(double x) { return 0.5 * Logarithm.ln((x + 1) / (x - 1)); }
 
-    public static double arsech(double x) { return this.l.ln(1 / x + Math.sqrt(1 / (x * x) - 1)); }
+    public static double arsech(double x) { return Logarithm.ln(1 / x + Math.sqrt(1 / (x * x) - 1)); }
 
-    public static double arcsch(double x) { return this.l.ln(1 / x + Math.sqrt(1 / (x * x) + 1)); }
+    public static double arcsch(double x) { return Logarithm.ln(1 / x + Math.sqrt(1 / (x * x) + 1)); }
 
     public static double arcsin(double x) { return Math.asin(x); }
 
