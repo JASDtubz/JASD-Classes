51303d5a25b0137e38748c9e6a8fb876cd7e0ac8
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-02-11 08:14:01 -0600

    Create BitOperator.java

---
+++ jasd/number/BitOperator.java
@@ -0,0 +1,12 @@
+package jasd.number;
+
+public class BitOperator
+{
+    public static int not(int x) { return ~x; }
+    
+    public static int and(int x, int y) { return x & y; }
+    
+    public static int or(int x, int y) { return x | y; }
+    
+    
+}
