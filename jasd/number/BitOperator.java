095ad66c5186205bb5924cf08beb68cfa2b36bed
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-02-11 08:51:37 -0600

    Update BitOperator.java

--- jasd/number/BitOperator.java
+++ jasd/number/BitOperator.java
@@ -16,7 +16,7 @@ public class BitOperator
     
     public static int xnor(int x, int y) { return ~(x ^ y); }
     
-    public static int right(int x, int y) { return ~x & y; }
+    public static int from(int x, int y) { return ~x & y; }
     
-    public static int left(int x, int y) { return x & ~y; }
+    public static int without(int x, int y) { return x & ~y; }
 }
