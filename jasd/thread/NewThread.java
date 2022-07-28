56cdedd23be590f5a44c3cedb5f485fa12c61417
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-07-27 23:07:12 -0500

    Create NewThread.java

---
+++ jasd/thread/NewThread.java
@@ -0,0 +1,14 @@
+package jasd.thread;
+
+public class NewThread
+{
+    public Runnable method;
+
+    public NewThread() { this.method = null; }
+
+    public NewThread(Runnable r) { this.method = r; }
+
+    public void execute() { this.execute(this.method); }
+
+    public void execute(Runnable method) { new Thread(new ThreadRunner(method)).start(); }
+}
