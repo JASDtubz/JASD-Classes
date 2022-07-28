7c02c185a791e3128eebdd2a316c8d5887d80761
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-07-28 09:29:15 -0500

    Update ThreadSleeper.java

--- jasd/thread/ThreadSleeper.java
+++ jasd/thread/ThreadSleeper.java
@@ -5,10 +5,10 @@ public class ThreadSleeper
     private ThreadSleeper() { }
 
     /**
-     * Makes an easy way to call a thread sleep.
+     * Makes an easy way to schedule a thread sleep.
      * @param millis Number of milliseconds the thread will sleep.
      */
-    public static void sleep(long millis)
+    public static void sleep(final long millis)
     {
         try { Thread.sleep(millis); }
         catch (InterruptedException ie) { ie.printStackTrace(); }
