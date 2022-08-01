6a6d7e94eefdff4708c780fde5ff3d18b8c3dcd2
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-07-31 22:45:26 -0500

    Update ArrayListPlus.java

--- jasd/lists/ArrayListPlus.java
+++ jasd/lists/ArrayListPlus.java
@@ -6,26 +6,51 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Random;
 
+/**
+ * ArrayListPlus is an extension of the ArrayList class that consists of less used methods.
+ * @param <C> Object list type.
+ */
 public class ArrayListPlus <C> extends ArrayList<C>
 {
+    /**
+     * Constructs a new ArrayListPlus object.
+     */
     public ArrayListPlus() { super(); }
 
-    public ArrayListPlus(Collection<? extends C> c) { super(c); }
+    /**
+     * Constructs a new ArrayListPlus object containing a Collection.
+     * @param c Starting list.
+     */
+    public ArrayListPlus(final Collection<? extends C> c) { super(c); }
 
-    public ArrayListPlus(int i) { super(i); }
+    /**
+     * Constructs a new ArrayListPlus object with an initial size.
+     * @param i Initial list size.
+     */
+    public ArrayListPlus(final int i) { super(i); }
 
-    public void shuffleList() { this.shuffleList(new Random()); }
+    /**
+     * Shuffles the list using the Random class.
+     * @return TRUE if the list has been scrambled.
+     */
+    public boolean shuffleList() { return this.shuffleList(new Random()); }
 
-    public void shuffleList(long seed) { this.shuffleList(new Random(seed)); }
+    /**
+     * Shuffles the list using the Random class with a seed.
+     * @param seed Seed for the Random class.
+     * @return TRUE if the list has been scrambled.
+     */
+    public boolean shuffleList(final long seed) { return this.shuffleList(new Random(seed)); }
 
     @SuppressWarnings(value = "unchecked")
-    private void shuffleList(Random r)
+    private boolean shuffleList(final Random r)
     {
         Object[] list = super.toArray();
+        final Object[] prev = list.clone();
 
         for (int i = 0; i < super.size(); i++)
         {
-            int index = r.nextInt(list.length);
+            final int index = r.nextInt(list.length);
             Object[] temp = new Object[list.length - 1];
 
             super.set(i, (C) list[index]);
@@ -35,24 +60,52 @@ public class ArrayListPlus <C> extends ArrayList<C>
 
             list = temp;
         }
+
+        return !Arrays.equals(prev, super.toArray());
     }
 
-    public void swap(int a, int b)
+    /**
+     * Swaps 2 items in the list.
+     * @param a Index of first item.
+     * @param b Index of second item.
+     * @return TRUE if the items have been swapped.
+     */
+    final public boolean swap(final int a, final int b)
     {
         C c = super.get(a);
 
         super.set(a, super.get(b));
         super.set(b, c);
+
+        return !super.get(a).equals(super.get(b));
     }
 
+    /**
+     * Swaps 2 items in the list.
+     * @param a Item in list to be swapped 1.
+     * @param b Item in list to be swapped 2.
+     * @return TRUE if the items have been swapped.
+     */
+    public boolean swap(C a, C b)
+    {
+        if (super.contains(a) && super.contains(b)) { return this.swap(super.indexOf(a), super.indexOf(b)); }
+
+        return false;
+    }
+
+    /**
+     * Sorts the hash codes of the objects in the list.
+     * @return TRUE if the items have been sorted.
+     */
     @SuppressWarnings(value = "unchecked")
-    public void sortByHashCode()
+    public boolean sortByHashCode()
     {
         final int length = super.size();
-        int[] codes = new int[length];
-        Object[] list = new Object[length];
+        final int[] codes = new int[length];
+        final Object[] list = new Object[length];
+        final Object[] prev = super.toArray();
 
-        for (int i = 0; i < length; i++) { codes[i] = super.get(i).hashCode(); System.out.println(codes[i]); }
+        for (int i = 0; i < length; i++) { codes[i] = super.get(i).hashCode(); }
 
         Arrays.sort(codes);
 
@@ -60,7 +113,7 @@ public class ArrayListPlus <C> extends ArrayList<C>
         {
             for (int j = 0; j < length; j++)
             {
-                C c = super.get(j);
+                final C c = super.get(j);
 
                 if (codes[i] == c.hashCode())
                 {
@@ -72,30 +125,41 @@ public class ArrayListPlus <C> extends ArrayList<C>
         }
 
         this.setList((C[]) list);
+
+        return !Arrays.equals(prev, super.toArray());
     }
 
+    /**
+     * Sorts the list using the Arrays.sort() method.
+     * @return TRUE if the list has been sorted.
+     */
     @SuppressWarnings(value = "unchecked")
     public boolean sort()
     {
-        Object[] list = super.toArray();
-        C c = (C) list[0];
+        final Object[] list = super.toArray();
+        final C c = (C) list[0];
+        final Object[] prev = super.toArray();
 
-        if (!(c instanceof Comparable))
-        {
-            this.sortByHashCode();
-
-            return false;
-        }
+        if (!(c instanceof Comparable)) { return this.sortByHashCode(); }
 
         Arrays.sort(list);
         this.setList((C[]) list);
 
-        return true;
+        return !Arrays.equals(prev, super.toArray());
     }
 
-    public void setList(C[] list)
+    /**
+     * Replaced the entire list with a new one.
+     * @param list List to replace current one.
+     * @return TRUE if the two lists are different.
+     */
+    public boolean setList(final C[] list)
     {
-        this.removeAll();
+        Object[] prev = super.toArray();
+
+        super.clear();
         Collections.addAll(this, list);
+
+        return !Arrays.equals(prev, super.toArray());
     }
 }
