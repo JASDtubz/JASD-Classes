7a7bfc00b6b6b66279e343494f56d007cef81bc4
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-08-17 17:39:46 -0500

    Update Logarithm.java

--- jasd/number/Logarithm.java
+++ jasd/number/Logarithm.java
@@ -1,12 +1,45 @@
 package jasd.number;
 
-public class Logarithm
+/**
+ * This class provides an easy way to call and declare all forms of logarithms.
+ *
+ * @author Jean-Denis Toting de Beauvoir
+ * @version 2022.8.17.17.39
+ */
+public final class Logarithm
 {
-    public static double log(double x, double y) { return Logarithm.lg(y) / Logarithm.lg(x); }
+    private Logarithm() { }
 
-    public static double lg(double x) { return Math.log10(x); }
+    /**
+     * Returns a logarithm with a base for a number.
+     * 
+     * @param base Base number of logarithm.
+     * @param value Value of the number.
+     * @return Log base of the number.
+     */
+    public static double log(final double base, final double value) { return Logarithm.lg(value) / Logarithm.lg(base); }
 
-    public static double ln(double x) { return Math.log(x); }
+    /**
+     * Returns the log base 10 of a value.
+     * 
+     * @param value Value of the number.
+     * @return Log base 10 of the number.
+     */
+    public static double lg(final double value) { return Math.log10(value); }
 
-    public static double lb(double x) { return Logarithm.log(2, x); }
+    /**
+     * Returns the log base e of a value.
+     * 
+     * @param value Value of the number.
+     * @return Log base e of the number.
+     */
+    public static double ln(final double value) { return Math.log(value); }
+
+    /**
+     * Returns the log base 2 of a value.
+     * 
+     * @param value Value of the number.
+     * @return Log base 2 of the number.
+     */
+    public static double lb(final double value) { return Logarithm.log(2.0D, value); }
 }
