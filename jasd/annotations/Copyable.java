ded6928a0fa164dc2e33ff64813528c536e82372
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-09-14 23:17:21 -0500

    Update Copyable.java

--- jasd/annotations/Copyable.java
+++ jasd/annotations/Copyable.java
@@ -1,1 +1,21 @@
-public @interface Copyable { }
+package jasd.annotations;
+
+/**
+ * This annotation is used for classes that have a method that makes a direct clone of their object, returning their
+ * type rather than {@code Object}.
+ *
+ * @author Jean-Denis Toting de Beauvoir
+ * @version 2022.9.14.23.17
+ */
+public @interface Copyable
+{
+   /**
+    * Used for the method {@code copy()} which returns a clone of their object.
+    */
+   boolean copyMethod() default false;
+
+   /**
+    * Used for the class with a {@code copy()} method that returns a clone of their object.
+    */
+   boolean hasCopyMethod() default false;
+}
