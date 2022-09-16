caa28b6e66ffde7a176fc4aebb7cf3c739a431ae
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-09-16 06:24:10 -0500

    Create DataList.java

---
+++ jasd/lists/DataList.java
@@ -0,0 +1,73 @@
+package jasd.lists;
+
+import jasd.annotations.Copyable;
+
+@Copyable(hasCopyMethod = true)
+public class DataList<C extends Number> extends ArrayListPlus<C>
+{
+    private static final long serialVersionUID = 1853461384L;
+
+    public DataList() { super(); }
+
+    public double arithmeticMean() { return this.sum() / super.size(); }
+
+    public double geometricMean()
+    {
+        double total = 0.0D;
+
+        for (C c : this) { total *= c.doubleValue(); }
+
+        return Math.pow(total, 1.0D / super.size());
+    }
+
+    public double median()
+    {
+        final DataList<C> list = this.copy();
+        final int two = list.size() / 2;
+
+        list.sort();
+
+        if (list.size() % 2 == 0) { return (list.get(two - 1).doubleValue() + list.get(two).doubleValue()) / 2; }
+        else { return list.get(two).doubleValue(); }
+    }
+
+    private double halfVariance()
+    {
+        final double mean = this.arithmeticMean();
+        double sum = 0;
+
+        for (C c : this) { sum += Math.pow(c.doubleValue() - mean, 2.0D); }
+
+        return sum;
+    }
+
+    public double populationVariance() { return this.halfVariance() / super.size(); }
+
+    public double sampleVariance() { return this.halfVariance() / (super.size() - 1); }
+
+    public double populationStandardDeviation() { return Math.sqrt(this.populationVariance()); }
+
+    public double sampleStandardDeviation() { return Math.sqrt(this.sampleVariance()); }
+
+    public double range()
+    {
+        final DataList<C> list = this.copy();
+
+        list.sort();
+
+        return list.get(list.size() - 1).doubleValue() - list.get(0).doubleValue();
+    }
+
+    public double sum()
+    {
+        double sum = 0.0D;
+
+        for (C c : this) { sum += c.doubleValue(); }
+
+        return sum;
+    }
+
+    @Copyable(copyMethod = true)
+    @SuppressWarnings(value = "unchecked")
+    public DataList<C> copy() { return (DataList<C>) this.clone(); }
+}
