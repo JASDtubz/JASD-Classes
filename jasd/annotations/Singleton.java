d62c3aa0daa026075f6b44725340f5e3df2c784d
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-09-22 14:02:25 -0500

    Update Singleton.java

--- jasd/annotations/Singleton.java
+++ jasd/annotations/Singleton.java
@@ -1,1 +1,8 @@
+/**
+ * This annotation is used for classes who use the Singleton pattern. (Only 1 instance of the class)
+ * 
+ * @author Jean-Denis Toting de Beauvoir
+ * @version 2022.09.22.14.01.57
+ */
+
 public @interface Singleton { }
